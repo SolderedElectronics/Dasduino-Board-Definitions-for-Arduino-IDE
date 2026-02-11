5bad27345f0fb2c50074db7d6791ededba9d923e
Josip Šimun Kuči <josipsimunkuci@gmail.com>
2026-02-11 13:19:53 +0100

    Fix wrong bootloader

--- Dasduino_megaTiny_core-3.0.0/bootloaders/optiboot_x/optiboot_x.c
+++ Dasduino_megaTiny_core-3.0.0/bootloaders/optiboot_x/optiboot_x.c
@@ -65,7 +65,7 @@
 /* Set bootloader baud rate.                              */
 /*                                                        */
 /* LED_START_FLASHES:                                     */
-/* Number of LED flashes on boot-up.                       */
+/* Number of LED flashes on bootup.                       */
 /*                                                        */
 /* LED_DATA_FLASH:                                        */
 /* Flash LED when transferring data. For boards without   */
